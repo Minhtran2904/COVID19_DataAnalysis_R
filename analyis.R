d38e1dcd599444a3face0fcad8dffbda7f7be849
Minhtran2904 <anhminh.2904@gmail.com>
2023-11-14 10:21:18 +0200

    Initial commit

---
+++ analyis.R
@@ -0,0 +1,25 @@
+rm(list=ls()) 
+library(Hmisc) 
+
+data <- read.csv("C:/Users/mykyt/Desktop/covid_R/COVID19_line_list_data.csv")
+describe(data) 
+
+
+data$death_dummy <- as.integer(data$death != 0)
+
+sum(data$death_dummy) / nrow(data)
+
+
+dead = subset(data, death_dummy == 1)
+alive = subset(data, death_dummy == 0)
+mean(dead$age, na.rm = TRUE)
+mean(alive$age, na.rm = TRUE)
+
+t.test(alive$age, dead$age, alternative="two.sided", conf.level = 0.99)
+
+men = subset(data, gender == "male")
+women = subset(data, gender == "female")
+mean(men$death_dummy, na.rm = TRUE)
+mean(women$death_dummy, na.rm = TRUE)
+
+t.test(men$death_dummy, women$death_dummy, alternative="two.sided", conf.level = 0.99)
